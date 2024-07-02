a604f3519d36b7413c2552ae2f13223a2cc3a115
Luthando496 <luthandodidiza197@gmail.com>
2024-07-02 16:57:22 +0200

    second commint

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Property 26",
   description: "Find The Perfect Property.",
+  keywords: ["property, real estate, rent, buy"],
 };
 
 export default function RootLayout({ children }) {
