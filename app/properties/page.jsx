fe1cf2269a0582144cd8b0b6222ca5c95741834b
Luthando496 <luthandodidiza197@gmail.com>
2024-07-02 18:54:33 +0200

    property card

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import properties from '@/properties.json'
+import PropertyCard from '@/components/PropertyCard'
+
 
 const Property = () => {
   return (
-    <h1>
-      Properties
-    </h1>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (<p>No properties found</p>) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.map(item =>(
+            <PropertyCard key={item._id} property={item} />
+          ))}
+        </div>
+        )}
+        
+      </div>
+    </section>
   )
 }
 
